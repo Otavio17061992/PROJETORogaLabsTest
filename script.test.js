e0d7618ad1ae90a718fcf8fdc97b96f6b65bb197
Otavio  Mesquita <93503467+Otavio17061992@users.noreply.github.com>
2022-05-11 22:34:53 -0300

    test finalizado e testado

--- script.test.js
+++ script.test.js
@@ -3,9 +3,9 @@ const get = require("./script");
 const path = "a.b.d[1].e";
 const myObj = { a: { b: { c: 1, d: [1, { e: 2 }] } } };
 
-// test("adds 1 + 2 to equal 3", () => {
-//     expect(sum(1, 2)).toBe(3);
-// });
+/*  test("adds 1 + 2 to equal 3", () => {
+     expect(sum(1, 2)).toBe(3);
+}); */
 
 test("return true", () => {
     expect(get(path, myObj)).toBe(2); // testa o retorno  2 do meu myObj 
